1e1f4cff84f1d404616c8f4a2dd124855b98b55c
LalbaAnthony <anthonylalaba24@gmail.com>
2025-11-16 18:14:17 +0100

    Suite

--- ressources/components/button/index.php
+++ ressources/components/button/index.php
@@ -1,11 +1,16 @@
 <?php
 
-use App\Icon;
+use App\Helpers;
+
+$types = ['text', 'password', 'email', 'number', 'url', 'tel', 'search', 'date', 'datetime-local', 'month', 'week', 'time', 'color', 'textarea'];
+$autocompletes = ['text' => 'on', 'password' => 'current-password', 'email' => 'email', 'tel' => 'tel', 'url' => 'url', 'search' => 'search', 'name' => 'name'];
+
+$id = isset($id) ? $id : Helpers::randomHex(8);
 
 ?>
 
 <button
-    id="<?= $id ?? '' ?>"
+    id="<?= $id ?>"
     type="<?= $type ?? 'button' ?>"
     class="button <?= $color ?? 'light' ?> <?= (isset($outline) && $outline) ? 'outline' : '' ?>"
     <?= (isset($href) && $href) ? 'onclick="window.location.href=\'' . $href . '\'"' : '' ?>>
