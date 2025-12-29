d6fdd2d1077c5fe56752592b93a9da5e06f3df07
LalbaAnthony <anthonylalaba24@gmail.com>
2025-12-29 15:22:58 +0100

    Suite

--- app/Controller/Controller.php
+++ app/Controller/Controller.php
@@ -8,7 +8,10 @@ abstract class Controller
 {
     use Utils;
 
+    /**
+     * Default values for pagination and sorting.
+     */
     const DEFAULT_PER_PAGE = 10;
     const DEFAULT_PAGE = 1;
-    const DEFAULT_SORT = [['column' => 'created_at', 'order' => 'DESC'], ['column' => 'id', 'order' => 'DESC']];
+    const DEFAULT_SORT = [['column' => 'created_at', 'order' => 'DESC'], ['column' => 'updated_at', 'order' => 'DESC']];
 }
